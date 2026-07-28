6999894272ee63d70abb7219c128a14ce433a472
junyangchan <kardxsg@gmail.com>
2026-07-28 18:13:23 +0800

    add google login

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -26,7 +26,7 @@ const privyConfig: PrivyClientConfig = {
       "wallet_connect_qr_solana",
     ],
   },
-  loginMethods: ["wallet", "email"],
+  loginMethods: ["google", "wallet"],
   embeddedWallets: {
     solana: { createOnLogin: "users-without-wallets" },
   },
